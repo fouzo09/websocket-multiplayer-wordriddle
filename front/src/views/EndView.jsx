7fa826c6c3936007a72597ea4d402ffd3916454e
Mafouz DIALLO <mafouzdiallo@gmail.com>
2022-09-24 11:18:40 +0000

    Build end page

--- front/src/views/EndView.jsx
+++ front/src/views/EndView.jsx
@@ -1,10 +1,43 @@
 import React from 'react'
+import { Button, Card, Content } from '../style';
 import GlobalView from './GlobalView'
+import winnerTrophy from '../assets/trophy.png'
+import lost from '../assets/lost.png'
 
 function EndView() {
-  return (
-    <div>EndView</div>
-  )
+
+   const status = 0;
+   let content = '';
+   
+
+  
+    if(status === 0){
+       content = (<>
+        <h2>Toutes nos felicitations, vous avez gagné !!!!!</h2>
+            <img src={winnerTrophy} alt='Lancer le dé' style={{width:'100px', height:'100px', marginBottom: '100px'}} /> 
+       </>)
+    }
+
+    if(status === 1){
+      content = (<>
+        <h2>Vous avez perdu la partie, courage</h2>
+      <img src={lost} alt='Lancer le dé' style={{width:'100px', height:'100px', marginBottom: '100px'}} />
+      </>)
+    }
+
+    return (
+      <>
+        <Content>
+          <Card width='600px' height='500px'>
+               {content}
+            <Button className='start'>
+              Relancer le jeu
+            </Button>
+          </Card>
+        </Content>
+      </>
+    );
+ 
 }
 
 export default GlobalView(EndView)
